30aac433e78473f24c0669c4caddd98193003d8c
Josh <richardsonjosh03@gmail.com>
2024-12-05 21:25:14 -0800

    put the homepage in a scrollview

--- Tabby/app/index.tsx
+++ Tabby/app/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, ScrollView, Dimensions } from "react-native";
 import { Link } from "expo-router";
 import { getAllCategories } from "@/database/databaseOperations";
 import { useEffect, useState } from "react";
@@ -32,31 +32,40 @@ const WelcomeScreen = () => {
     }
   }, [router]);
 
+  const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
+  const isSmallPhone = screenWidth <= 400 && screenHeight <= 890;
+
+  const dynamicStyles = {
+    ScrollViewMaxHeight: isSmallPhone ? 800 : undefined,
+  };
+
   const handleShowingLoading = () => {
     if (isLoading) {
       return <View className="flex-1 justify-center items-center bg-[#1E1E1E] h-full" style={{ paddingBottom: insets.bottom }}>
         <LoadingSpinner />
       </View>
     } else {
-      return <View className="flex-1 justify-center items-center bg-[#1E1E1E] h-full" style={{ paddingBottom: insets.bottom }}>
-        <Text className="text-4xl font-bold mb-4 text-white">
-          Welcome to Tabby
-        </Text>
-        <Image source={require("@/assets/icons/app/adaptive-icon.png")} className="w-48 h-48" testID="settings-image" />
-        <Text className="text-lg text-center mb-8 text-white">
-          Scan books and store your book information effortlessly.
-        </Text>
-        <Link
-          className="bg-blue-600 py-2 px-4 rounded text-white text-lg font-semibold"
-          href={"/library"}
-          testID="get-started-button" // Add testID here
-        >
-          Get Started
-        </Link>
-        <Image source={require("@/assets/icons/app/sleeping_cat_3.png")} className="w-48 h-48 bottom-0 inset-x-0" testID="settings-image" />
-      </View>
-    }
+      return <ScrollView className="" contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }} style={{ maxHeight: dynamicStyles.ScrollViewMaxHeight }}>
+        <View className="flex-1 justify-center items-center bg-[#1E1E1E] h-full" style={{ paddingBottom: insets.bottom }}>
 
+          <Text className="text-4xl font-bold mb-4 text-white">
+            Welcome to Tabby
+          </Text>
+          <Image source={require("@/assets/icons/app/adaptive-icon.png")} className="w-48 h-48" testID="settings-image" />
+          <Text className="text-lg text-center mb-8 text-white">
+            Scan books and store your book information effortlessly.
+          </Text>
+          <Link
+            className="bg-blue-600 py-2 px-4 rounded text-white text-lg font-semibold"
+            href={"/library"}
+            testID="get-started-button" // Add testID here
+          >
+            Get Started
+          </Link>
+          <Image source={require("@/assets/icons/app/sleeping_cat_3.png")} className="w-48 h-48 bottom-0 inset-x-0" testID="settings-image" />
+        </View>
+      </ScrollView>
+    }
   };
 
   return (
